908c8ff056d5bad7e90a6ac212ed2603f5568782
Nguyễn Hoàng Khang <khangnguyenhoang1802@gmail.com>
2025-10-21 22:16:28 +0700

    Add files via upload

--- b9ss3.c
+++ b9ss3.c
@@ -3,16 +3,10 @@
 int main() {
     int n;
 
-    do {
-        printf("Nhap n (>1): ");
-        scanf("%d", &n);
+    printf("Nhap n (>1): ");
+    scanf("%d", &n);
 
-        if (n <= 1) {
-            printf("Loi: n phai la so nguyen lon hon 1. Vui long nhap lai.\n");
-        }
-    } while (n <= 1);
-
-    double A = (sqrt(2 * n + 1) - 1.0) / 2.0;
+    double A = 1.0/((n-1.0)*n) + 1.0/(n*(n+1.0)) + 1.0/((n+1.0)*(n+2.0));
 
     printf("A = %.5f\n", A);
 
